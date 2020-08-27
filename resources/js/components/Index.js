dc45e7ec81f8817571f36f03a84f9349c487f45f
Amarendra Sharma <s.amarendra1995@gmail.com>
2020-08-27 23:50:18 +0530

    update

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,24 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
-function Index() {
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+import Main from './Router';
+class Index extends Component {
+  render() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Index Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <BrowserRouter>
+        <Route component={Main} />
+      </BrowserRouter>
     );
+  }
 }
-
-export default Index;
-
-if (document.getElementById('example')) {
-    ReactDOM.render(<Index />, document.getElementById('example'));
-}
+ReactDOM.render(<Index/>, document.getElementById('index'));
